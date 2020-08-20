09b9293a65ee13599bdf5c45f46a5d0cb1bcc076
Paulo Cardoso <paulo_jcs@yahoo.com.br>
2020-08-20 12:50:56 -0300

    deploy

---
+++ Funcao/Funcao/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace Funcao
+{
+
+    // AQUI VEREMOS COMO FAZER UMA FUNÇÂO EM C#
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("--- INFORME 3 NUMEROS ---");
+            Console.Write("Digite o Primeiro Numero: ");
+            int n1 = int.Parse(Console.ReadLine());
+            Console.Write("Digite o Segundo Numero: ");
+            int n2 = int.Parse(Console.ReadLine());
+            Console.Write("Digite o Terceiro Numero: ");
+            int n3 = int.Parse(Console.ReadLine());
+
+            int maiorEscolhido = NumeroMaior(n1, n2, n3);
+
+            Console.WriteLine("O Maior Numero Escolhido Foooiiiii o " + maiorEscolhido);
+
+        }
+
+        // AQUI VAMOS FAZER UMA FUNÇAO 
+
+        static int NumeroMaior(int a, int b, int c)
+        {
+            int maior = 0;
+            if (a > b && a > c)
+            {
+                maior = a;
+            }
+            else if (b > a && b > c)
+            {
+                maior = b;
+            }
+            else if (c > a && c > b)
+            {
+                maior = c;
+            }
+            else
+            {
+                maior = 0;
+            }
+
+            return maior;
+
+        }
+    }
+}
---
+++ MedidaDoTriangulo/MedidaDoTriangulo/Program.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Globalization;
+using System.Xml.Linq;
+
+namespace MedidaDoTriangulo
+{
+    class Program
+    {
+
+        /* calculo para area do triangulo ( formula de Heron) 
+         *          ______________________________
+         *  area = V     p(p-a) (p-b) (p-c)
+         * 
+         *      onde
+         * 
+         *         a + b + c    
+         *  p = --------------
+         *             2
+         * 
+         */
+
+        static void Main(string[] args)
+        {
+            double x1, x2, x3, y1, y2, y3, medidaX, medidaY;
+
+            Console.WriteLine("------Entre com as medidas do TRIANGULO X -------");
+            Console.Write("Lado 01 = ");
+            x1 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Lado 02 = ");
+            x2 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Lado 03 = ");
+            x3 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+
+            Console.WriteLine("------Entre com as medidas do TRIANGULO Y -------");
+            Console.Write("Lado 01 = ");
+            y1 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Lado 02 = ");
+            y2 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Lado 03 = ");
+            y3 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+
+            Console.WriteLine("------ Resultado -------");
+            medidaX = TrianguloX(x1, x2, x3);
+            Console.WriteLine("areaX é >>>>>> " + medidaX.ToString("F4"));
+
+            medidaY = TrianguloY(y1, y2, y3);
+            Console.WriteLine("area Y é >>>>>> " + medidaY.ToString("F4"));
+
+            Console.WriteLine("------ A Maior área é -------");
+            if (medidaX > medidaY)
+            {
+                Console.WriteLine("Area X é maior que a area Y");
+            } else if (medidaX < medidaY)
+            {
+                Console.WriteLine("Area Y é maior que a area X");
+            } else
+            {
+                Console.WriteLine("Area X é igual a area Y");
+            }
+
+
+        }
+
+        static double TrianguloX(double xa, double xb, double xc)
+        {
+            /* vamos calcular valor de P
+             *       a + b + c
+             * p = --------------
+             *           2
+             */
+            
+            double p = (xa + xb + xc) / 2;
+
+
+            /* vamos calcular a raiz quadrada 
+             *      ______________________________
+             *  area = V     p(p-a) (p-b) (p-c)
+             */
+
+            double areaX = Math.Sqrt(p *(p-xa)*(p-xb)*(p-xc));
+
+            return areaX;
+
+        }
+
+        static double TrianguloY(double ya, double yb, double yc)
+        {
+            /* vamos calcular valor de P
+            *       a + b + c
+            * p = --------------
+            *           2
+            */
+
+            double p = (ya + yb + yc) / 2;
+
+
+            /* vamos calcular a raiz quadrada 
+             *      ______________________________
+             *  area = V     p(p-a) (p-b) (p-c)
+             */
+
+            double areaY = Math.Sqrt(p * (p - ya) * (p - yb) * (p - yc));
+
+            return areaY;
+
+
+        }
+    }
+}
