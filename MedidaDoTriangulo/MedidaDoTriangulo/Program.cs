2cd2307b21a1d6347455783affa04ad19f748af7
Paulo Cardoso <paulo_jcs@yahoo.com.br>
2020-08-20 14:05:32 -0300

    atualizacao

--- MedidaDoTriangulo/MedidaDoTriangulo/Program.cs
+++ MedidaDoTriangulo/MedidaDoTriangulo/Program.cs
@@ -23,29 +23,33 @@ namespace MedidaDoTriangulo
         {
             double x1, x2, x3, y1, y2, y3, medidaX, medidaY;
 
+            Triangulo x, y;
+            x = new Triangulo();
+            y = new Triangulo();
+
             Console.WriteLine("------Entre com as medidas do TRIANGULO X -------");
             Console.Write("Lado 01 = ");
-            x1 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            x.A = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
             Console.Write("Lado 02 = ");
-            x2 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            x.B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
             Console.Write("Lado 03 = ");
-            x3 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            x.C = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
 
 
             Console.WriteLine("------Entre com as medidas do TRIANGULO Y -------");
             Console.Write("Lado 01 = ");
-            y1 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            y.A = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
             Console.Write("Lado 02 = ");
-            y2 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            y.B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
             Console.Write("Lado 03 = ");
-            y3 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            y.C = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
 
 
             Console.WriteLine("------ Resultado -------");
-            medidaX = CalculaArea(x1, x2, x3);
+            medidaX = x.CalculaArteaTriangulo();
             Console.WriteLine("areaX é >>>>>> " + medidaX.ToString("F4"));
 
-            medidaY = CalculaArea(y1, y2, y3);
+            medidaY = y.CalculaArteaTriangulo();
             Console.WriteLine("area Y é >>>>>> " + medidaY.ToString("F4"));
 
             Console.WriteLine("------ A Maior área é -------");
@@ -62,28 +66,6 @@ namespace MedidaDoTriangulo
 
             Console.ReadLine();
             
-
-        }
-
-        static double CalculaArea(double a, double b, double c)
-        {
-            /* vamos calcular valor de P
-             *       a + b + c
-             * p = --------------
-             *           2
-             */
-            
-            double p = (a + b + c) / 2;
-
-
-            /* vamos calcular a raiz quadrada 
-             *      ______________________________
-             *  area = V     p(p-a) (p-b) (p-c)
-             */
-
-            double area = Math.Sqrt(p *(p-a)*(p-b)*(p-c));
-
-            return area;
 
         }
 
---
+++ MedidaDoTriangulo/MedidaDoTriangulo/Triangulo.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+
+namespace MedidaDoTriangulo
+{
+    class Triangulo
+    {
+        public double A;
+        public double B;
+        public double C;
+
+        public double CalculaArteaTriangulo()
+        {
+
+            Console.WriteLine(A + "-" + B + "-" + C);
+
+            /* vamos calcular valor de P
+             *       a + b + c
+             * p = --------------
+             *           2
+             */
+
+            double p = (A + B + C) / 2;
+
+
+            /* vamos calcular a raiz quadrada 
+             *      ______________________________
+             *  area = V     p(p-a) (p-b) (p-c)
+             */
+
+            double area = Math.Sqrt(p * (p - A) * (p - B) * (p - C));
+
+            return area;
+
+        }
+    }
+}
