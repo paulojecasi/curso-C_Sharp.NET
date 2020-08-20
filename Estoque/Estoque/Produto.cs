a66f1c776cb3b49c75cdfc5cc3073c3070c8e9d1
Paulo Cardoso <paulo_jcs@yahoo.com.br>
2020-08-20 19:52:48 -0300

    atualizacao

---
+++ Estoque/Estoque/Produto.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Globalization;
+
+namespace Estoque
+{
+    class Produto
+    {
+
+        public string Nome;
+        public double Preco;
+        public int Quantidade;
+
+        public double ValorTotalEmEstoque()
+        {
+            double precoUnitario = Preco;
+            double precoTotal = (Preco * Quantidade);
+
+            return precoTotal;
+
+        }
+
+        public void AdicionarProdutos(int quantidade)
+        {
+            Quantidade = (Quantidade + quantidade);
+        }
+
+        public void RemoverProdutos(int quantidade)
+        {
+            Quantidade = (Quantidade - quantidade);
+        }
+
+
+        public override string ToString()
+        {
+            return 
+               "Descrição: "    + Nome                                     + " - " +
+               " Vl Unitario: " + Preco.ToString("F2", 
+                                    CultureInfo.InvariantCulture)          + " - " +
+               " Quantidade: "  + Quantidade              + " Unidades - " +
+               " Vl Total: "    + ValorTotalEmEstoque().ToString("F2", 
+                                    CultureInfo.InvariantCulture);
+        }
+
+
+    }
+}
