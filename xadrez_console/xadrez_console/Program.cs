46bd05901ea19da02599f8a3344bc356b8904bfb
Paulo Cardoso <paulo_jcs@yahoo.com.br>
2020-08-25 12:28:13 -0300

    criando jogo xadrez

---
+++ xadrez_console/xadrez_console/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using tabuleiro;
+
+namespace xadrez_console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Posicao P = new Posicao(3,4);
+
+            Console.WriteLine("Posicao: " + P);
+            Console.ReadLine();
+        }
+    }
+}
